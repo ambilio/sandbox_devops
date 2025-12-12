00ae8ffe57fb4081c0d8b96f40dff7e9303cec73
Nikhil Kumar <--nikhil@mi-webworks.com>
2025-12-12 20:00:37 +0530

    mysql shell scripting changed to work fine

--- Embeddings/mysql_embedding/start.sh
+++ Embeddings/mysql_embedding/start.sh
@@ -3,7 +3,6 @@ set -e
 
 nginx
 
-# locales (fix LC_ALL error)
 apt-get update && apt-get install -y locales
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
@@ -11,5 +10,4 @@ locale-gen
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-# run ttyd as PID 1 so container stops when user disconnects
 exec ttyd -p 7681 -W bash -lc "mysqlsh"
